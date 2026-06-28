5c68525e89d594770328f265b35d644a1d4bbc90
toshiro-estrella <toshiroyohan15@gmail.com>
2026-06-28 12:11:18 -0500

    agregar config para web

---
+++ lib/config.dart
@@ -0,0 +1,4 @@
+class Config {
+  static const supabaseUrl = 'https://rxlngcfcfobkmodwurxe.supabase.co';
+  static const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4bG5nY2ZjZm9ia21vZHd1cnhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5MjQwNzgsImV4cCI6MjA2OTUwMDA3OH0.GTt-5QgK0Y9VAXc77ytk7uzcsnL9mh2TRGcJ73sDpkg';
+}
